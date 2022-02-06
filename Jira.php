04d6f91d6e936ad34957d59c463b389724e855c7
Tamás Gulácsi <tamas@gulacsi.eu>
2022-02-06 17:30:22 +0100

    Add attachments

--- Jira.php
+++ Jira.php
@@ -14,6 +14,8 @@ require_once( dirname(__FILE__).'/core/jira_api.php' );
 
 require_api( 'install_helper_functions_api.php' );
 require_api( 'authentication_api.php');
+require_api( 'bugnote_api.php' );
+require_api( 'file_api.php' );
 
 class JiraPlugin extends MantisPlugin {
 	function register() {
@@ -45,37 +47,81 @@ class JiraPlugin extends MantisPlugin {
 		);
 	}
 
-	function update_bug( $p_event_name, $p_params ) {
+	function update_bug( $p_event_name, $p_bug_id ) {
+		//$conf = $this->config();
+		//$iss = create_issue_service($conf['jira_host'], $conf['jira_token']);
 	}
 
-	function bugnote_add( $p_event_name, $p_params ) {
+	function bugnote_add( $p_event_name, $p_bug_id ) {
+		$t_bugnote_id = bugnote_get_latest_id( $p_bug_id );
+		$t_bugnote = bugnote_get( $t_bugnote_id );
+		if( VS_PUBLIC == $t_bugnote->view_state ) {
+			$conf = $this->config();
+			$iss = create_issue_service($conf['jira_host'], $conf['jira_token']);
+			if( strlen($t_bugnote->note) !== 0 ) {
+				$iss->addComment($issueKey, jira_new_comment($t_bugnote->note));
+			}
+			$t_tempdir = sys_get_temp_dir();
+			$t_attachments = file_get_visible_attachments( $p_bug_id );
+			$t_files = array();
+			foreach( $t_attachments as $t_file ) {
+				if( $t_file['download_url'] && $t_file['diskfile'] && $t_file['bugnote_id'] == $t_bugnote_id ) {
+					$t_bn = basename($t_file['display_name']);
+					$t_ext = strrchr($t_bn, '.');
+					if( $t_ext ) {
+						$t_bn = substr( $t_bn, 0, -strlen($t_ext) );
+					} else {
+						$t_ext = '';
+					}
+					$t_files[] = secure_named_symlink('', $t_file['diskfile'], $t_file['display_name']);
+				}
+			}
+			if( count($t_files) != 0 ) {
+				$iss->addAttachment($issueKey, jira_new_attachments($t_files));
+				foreach( $t_files as $t_file ) {
+					if( file_exists($t_file) && filetype($t_file) == 'link' ) {
+						unlink($t_file);
+					}
+				}
+			}
+		}
 	}
 
-	function schema() {
-		$opts = array(
-			'mysql' => 'DEFAULT CHARSET=utf8',
-			'pgsql' => 'WITHOUT OIDS'
-		);
-		return array(
-			array( 'CreateTableSQL', array( plugin_table( 'current' ), "
-				bug_id		I	NOTNULL UNSIGNED,
-				user_id		I	NOTNULL UNSIGNED,
-				cents		I	NOTNULL UNSIGNED",
-				$opts)
-			),
-			array( 'CreateIndexSQL', array( 'idx_contributors_bugid', plugin_table( 'current' ), 'bug_id' ) ),
+}
 
-			array( 'CreateTableSQL', array( plugin_table( 'history' ) , "
-				id			I	NOTNULL UNSIGNED PRIMARY AUTOINCREMENT,
-				modifier_id	I	NOTNULL UNSIGNED,
-				modified_at	I	NOTNULL DEFAULT 0,
-				bug_id		I	NOTNULL UNSIGNED,
-				user_id		I	NOTNULL UNSIGNED,
-				cents		I	NOTNULL UNSIGNED",
-				$opts)
-			),
-		);
+function secure_named_symlink($dir, $target, $name) {
+	if( !(isset($dir) && is_string($dir) && $dir) ) {
+		$dir = sys_get_temp_dir();
 	}
+	$name = basename( $name );
+	$postfix = strrchr($name, '.');
+	if( $postfix ) {
+		$name = substr($name, 0, -strlen($postfix));
+	} else {
+		$postfix = '';
+	}
+
+    // find a temporary name
+    $tries = 1;
+    do {
+        // get a known, unique temporary file name
+        $sysFileName = tempnam($dir, $prefix);
+        if ($sysFileName === false) {
+            return false;
+        }
+
+        // tack on the extension
+        $newFileName = $sysFileName . $postfix;
+		$ok = symlink( $target, $name . $postfix );
+		unlink( $sysFileName );
+		if( $ok ) {
+			return $newFileName;
+        }
+
+        $tries++;
+    } while ($tries <= 5);
+
+    return false;
 }
 
 // vim: set noet:
--- core/jira_api.php
+++ core/jira_api.php
@@ -5,13 +5,18 @@ use JiraRestApi\Configuration\ArrayConfiguration;
 use JiraRestApi\Issue\IssueService;
 
 function create_issue_service($p_host, $p_token) {
-
     return new IssueService(new ArrayConfiguration(
               array(
                    'jiraHost' => $p_host,
                     'tokenBasedAuth' => true,
                     'personalAccessToken' => $p_token,
-                    'jiraRestApiV3' => true,
+                    'useV3RestApi' => true,
               )
        ));
+}
+
+function jira_new_comment($p_body) {
+    $comment = new Comment();
+    $comment->setBody($p_body);
+    return $comment;
 }
